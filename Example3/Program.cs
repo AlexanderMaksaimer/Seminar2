2da82aa818f0233091e1f70dc5aa9d5f6b7e9054
«Aleksander <pavel4uk18@gmail.com>
2022-05-13 20:16:49 +0300

    Example3

---
+++ Example3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому
+//34, 5 ->  Не кратно, остаток 4
+//16, 4 -> кратно
+
+Console.WriteLine("Введите первое число: ");
+string firstNumberStr = Console.ReadLine();
+Console.WriteLine("Введите второе число: ");
+string secondNumberStr = Console.ReadLine();
+int first = int.Parse(firstNumberStr);
+int second = int.Parse(secondNumberStr);
+
+int result = first % second;
+
+if(result == 0)
+    Console.WriteLine ("кратно");
+else
+    Console.WriteLine ($"некратно, остаток {result}");
