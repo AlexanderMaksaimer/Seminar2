5d4c0d877d9dbcb9200bb8ac738a9694001b062a
«Aleksander <pavel4uk18@gmail.com>
2022-05-13 19:51:32 +0300

    Example2

---
+++ Example2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+int number = new Random ().Next(100,1000);
+string strr = number.ToString();
+
+Console.WriteLine($"{number} -> {strr[0]}{strr[2]}");
